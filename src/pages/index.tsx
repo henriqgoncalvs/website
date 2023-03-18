740f06d36cd2c3cf03002776c72636e6b0301f54
Henrique <henriquesg09@hotmail.com>
2023-03-18 14:28:58 -0300

    feat: adjustments on profile banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,125 @@
-import { Atom, Cake, MapPin } from '@phosphor-icons/react';
+import {
+  Atom,
+  Cake,
+  CaretUp,
+  CaretDown,
+  MapPin,
+  LinkSimple,
+  GithubLogo,
+  LinkedinLogo,
+  MusicNote,
+} from '@phosphor-icons/react';
+import clsx from 'clsx';
 import Image from 'next/image';
 import Link from 'next/link';
-import { type ReactElement } from 'react';
+import { useState, type ReactElement } from 'react';
 
 import { Layout } from '@/ui/layout';
 
 const ProfileBanner = () => {
+  const [bannerExpanded, setBannerExpanded] = useState(false);
+
   return (
     <div className="flex flex-col items-start text-xl">
-      <div className="h-40 w-full overflow-hidden rounded-2xl bg-midnight">
+      <div
+        className={clsx(
+          'relative w-full overflow-hidden rounded-2xl bg-midnight',
+          bannerExpanded ? 'h-96' : 'h-40'
+        )}
+      >
         <Image
           src="/images/hubble-wp.jpg"
-          className="rounded-2xl"
+          className="absolute top-0 -translate-y-[130px] rounded-2xl"
           width="755"
           height="726"
           alt="A picture the hubble satellite took on March 20th, 1996"
         />
+
+        {bannerExpanded && (
+          <Link
+            href="https://hubblesite.org/contents/media/images/2000/34/1007-Image.html"
+            className="absolute top-6 left-6 flex items-center gap-2 rounded-2xl bg-neutral-900/40 p-4 text-sm text-white"
+          >
+            <LinkSimple size={16} />
+            <span>Colliding Galaxies NGC 6745</span>
+          </Link>
+        )}
       </div>
 
       <div className="z-10 flex flex-col gap-4 px-4">
-        <div className="flex w-full items-center justify-between">
-          <div className="-mt-16 w-fit rounded-full bg-midnight p-2 ring-[5px] ring-zinc-600/10">
-            <Image
-              src="https://avatars.githubusercontent.com/u/25696006?v=4"
-              className="h-32 w-32 rounded-full bg-midnight"
-              width="160"
-              height="160"
-              alt="Profile picture"
-            />
+        <div
+          className={clsx(
+            'flex w-full items-center justify-between',
+            bannerExpanded && 'pb-5'
+          )}
+        >
+          <div className="flex items-end gap-2">
+            <div
+              className={clsx(
+                '-mt-16 w-fit rounded-full bg-midnight p-2  ring-zinc-600/10',
+                bannerExpanded ? 'ring-[5px]' : 'ring-4'
+              )}
+            >
+              <Image
+                src="https://avatars.githubusercontent.com/u/25696006?v=4"
+                className={clsx(
+                  'h-32 w-32 rounded-full bg-midnight',
+                  bannerExpanded ? 'h-32 w-32' : 'h-24 w-24'
+                )}
+                width="160"
+                height="160"
+                alt="Profile picture"
+              />
+            </div>
+
+            <Link
+              href="https://github.com/hnqg"
+              target="_blank"
+              className="rounded-xl bg-zinc-700 p-2"
+            >
+              <GithubLogo size={bannerExpanded ? 24 : 20} />
+            </Link>
+
+            <Link
+              href="https://www.linkedin.com/in/henriiqueg/"
+              target="_blank"
+              className="rounded-xl bg-zinc-700 p-2"
+            >
+              <LinkedinLogo size={bannerExpanded ? 24 : 20} />
+            </Link>
           </div>
 
-          <div className="space-x-3 pb-10 text-sm font-bold">
-            <Link href="/about" passHref>
-              About
-            </Link>
-            <Link href="/about" passHref>
-              Dashboard
-            </Link>
-            <Link href="/about" passHref>
-              Blog
-            </Link>
-            <Link href="/about" passHref>
-              Guestbook
-            </Link>
+          <div className="-mt-5 flex flex-col items-end justify-between gap-3">
+            <button
+              className="rounded-full bg-zinc-700 p-3"
+              onClick={() => setBannerExpanded((prev) => !prev)}
+            >
+              {bannerExpanded ? (
+                <CaretUp size={16} />
+              ) : (
+                <CaretDown size={16} />
+              )}
+            </button>
+
+            <div
+              className={clsx(
+                'gap-2 space-x-3  pb-5 font-bold',
+                bannerExpanded ? 'text-base' : 'text-sm'
+              )}
+            >
+              <Link href="/about" passHref>
+                About
+              </Link>
+              <Link href="/dashboard" passHref>
+                Dashboard
+              </Link>
+              <Link href="/blog" passHref>
+                Blog
+              </Link>
+              <Link href="/guestbook" passHref>
+                Guestbook
+              </Link>
+            </div>
           </div>
         </div>
 
@@ -55,13 +132,19 @@ const ProfileBanner = () => {
           </code>
         </h1>
 
-        <p className="flex flex-col gap-2 text-lg">
-          Usual metalhead with an obsession for code. I like to
-          build cool stuff for the web using top notch
-          technologies.
+        <p
+          className={clsx(
+            bannerExpanded ? 'text-lg' : 'text-base'
+          )}
+        >
+          <span className="underline decoration-neutral-700 decoration-wavy decoration-2">
+            Usual metalhead
+          </span>{' '}
+          with an obsession for code. I like to build cool stuff
+          for the web using top notch technologies.
         </p>
 
-        <div className="flex w-full items-center justify-start gap-6 border-y-[1px] border-gray-600/20 py-3 text-sm">
+        <div className="flex w-full items-center justify-between gap-3 border-y-[1px] border-gray-600/20 py-3 text-sm font-bold">
           <div className="flex items-center gap-1">
             <Atom size={16} />
             <span>Front dev @ PagSeguro</span>
@@ -74,7 +157,12 @@ const ProfileBanner = () => {
 
           <div className="flex items-center gap-1">
             <Cake size={16} />
-            <span>March 20th 🐟</span>
+            <span>March 20th</span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <MusicNote size={16} />
+            <span>Drummer</span>
           </div>
         </div>
       </div>
