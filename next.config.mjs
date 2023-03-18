e878a59d640deabe5232e29c5cd4b1a1d0a3b767
Henrique <henriquesg09@hotmail.com>
2023-03-18 03:43:22 -0300

    feat: profile banner on home

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
  */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+!process.env.SKIP_ENV_VALIDATION &&
+  (await import('./src/env.mjs'));
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -15,8 +16,16 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
   },
 };
 export default config;
---
+++ src/ui/layout.tsx
@@ -0,0 +1,40 @@
+import { type ReactNode } from 'react';
+
+// import Starfield from '@/ui/starfield';
+
+export const Layout = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  return (
+    <>
+      <svg
+        className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light"
+        width="100%"
+        height="100%"
+        id="texture"
+      >
+        <filter id="noise">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.7"
+            numOctaves="12"
+            stitchTiles="stitch"
+          />
+        </filter>
+        <rect
+          width="100%"
+          height="100%"
+          filter="url(#noise)"
+        ></rect>
+      </svg>
+
+      <main className="mx-auto min-h-screen max-w-[640px] px-4 py-10">
+        {children}
+      </main>
+
+      {/* <Starfield /> */}
+    </>
+  );
+};
